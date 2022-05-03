6bf216c6ef063be254546c284a6290465f3e812f
sa0276144 <0276144@bdeb.qc.ca>
2022-05-03 14:17:19 -0400

    fixed fact function

--- factGUI.py
+++ factGUI.py
@@ -2,15 +2,15 @@ from tkinter import *
 
 def calculer():
     text1 = entry1.get()
-    text2 = entry2.get()
-    if (text1.strip() != "" and text2.strip() != "" and text3.strip() != ""):
-        sv.set()
+    if text1.isnumeric():
+        fact = factorial(int(text1))
+        entry2.insert(END, str(fact))
 
 def factorial(n):
     if n <= 1:
         return 1
     else:
-        return n * factorial(n-1) 
+        return n * factorial(n-1)
 
 def effacer():
     entry1.delete(0, END)
@@ -31,8 +31,7 @@ entry1 = Entry(window, width=20)
 entry2 = Entry(window, width=20)
 
 button1 = Button(window, text="Effacer le contenu", command=effacer)
-button2 = Button(window, text="Calculer la factorielle", command=factorial)
-button3 = Button(window, text="Quitter", command=window.quit)
+button2 = Button(window, text="Calculer la factorielle", command=calculer)
 
 label1.grid(row = 0, column = 0, sticky = E, padx = 15, pady = 15)
 label2.grid(row = 1, column = 0, sticky = E, padx = 15, pady = 15)
@@ -42,6 +41,4 @@ entry1.grid(row = 0, column = 1, sticky = E, padx = 15, pady = 15)
 entry2.grid(row = 1, column = 1, sticky = E, padx = 15)
 button2.grid(row = 4, column = 1, sticky = E, pady = 15)
 
-#window.mainloop()
-
-print(factorial(5))
+window.mainloop()
